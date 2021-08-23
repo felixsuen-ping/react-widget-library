c116f35ac5d4247ac6de145d2de3101e9f77c391
Felix Suen <felixsuen@pingidentity.com>
2021-08-23 15:24:21 -0700

    Mark got it fixed

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Recaptcha from './Recaptcha'
+import { Widget } from 'widget-react';
+//import Recaptcha from './Recaptcha'
 
-window.onloadCallBack = ReactDOM.render(<Recaptcha />, document.getElementById('root'))
+window.onloadCallBack = ReactDOM.render(<Widget baseUrl="https://localhost:9031"/>, document.getElementById('root'))
--- src/Widget.js
+++ src/Widget.js
@@ -4,48 +4,22 @@ import AuthnWidget from "@ping-identity/pf-authn-js-widget";
 
 export const Widget = (props) => {
 
-  var authnWidget;
-
-  function checkRecaptcha(token) {
-    console.log('captcha response: ' + token);
-    if (token.length === 0) {
-        //reCaptcha not verified
-        authnWidget.clearPendingState();
-        console.log('did not pass captcha try again');
-    } else {
-        authnWidget.dispatchPendingState(token);
-    }
-}
-
-  function invokeReCaptcha() {
-      console.log(this.props.grecaptcha);
-      let token = this.props.grecaptcha.getResponse();
-      if (token) {
-          checkRecaptcha(token);
-      }
-      else {
-          this.props.grecaptcha.execute();
-      }
-  }
-
   useEffect(() => {
-    // creating the options object
     let options = { divId: "authnwidget" };
     if (props.flowId)
       options = { ...options, flowId: props.flowId };
     if (props.logo)
       options = { ...options, logo: props.logo };
-    if (props.invokeReCaptcha)
-      options = { ...options, invokeReCaptcha: {invokeReCaptcha} };
-    if (props.checkRecaptcha)
-      options = { ...options, checkRecaptcha: "checkRecaptcha" };
-    if (props.grecaptcha)
-      options = { ...options, grecaptcha: props.grecaptcha };
+    // enable recaptcha
+    if (invokeReCaptcha)
+      options = { ...options, invokeReCaptcha: invokeReCaptcha, checkRecaptcha: "checkRecaptcha" };
+    if (grecaptcha)
+      options = { ...options, grecaptcha: grecaptcha };
     if (props.deviceProfileScript)
       options = { ...options, deviceProfileScript: props.deviceProfileScript };
     console.log(options);
 
-    var authnWidget = new AuthnWidget(props.baseUrl, options);
+    authnWidget = new AuthnWidget(props.baseUrl, options);
     if (props.redirectlessConfig)
       authnWidget.initRedirectless(props.redirectlessConfig);
     else
